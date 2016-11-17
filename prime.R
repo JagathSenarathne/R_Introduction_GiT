1f05a58b2c4390a2878db89181ab403f1af5ddfe
JagathSenarathne <jagath.gedara@hdr.qut.edu.au>
2016-11-17 16:30:42 +1000

    This is the first version

---
+++ prime.R
@@ -0,0 +1,33 @@
+
+Prime.func<-function(x)
+{
+  
+  y=rep(0,length(x))
+  for(j in 1:length(x))
+  {
+    x1=x[j]
+    
+    if(x1==1 | x1==2)
+      {
+        y[j]=0
+      }else{
+        for(i in 2:(x1-1)) 
+        {
+          if(x1 %% i == 0) 
+          {
+            y[j]=y[j]+1
+          }
+          
+        }
+      
+      }
+    
+  }
+  
+ 
+  out=ifelse(y>0,"not a prime", "prime")
+  print(out)
+  
+}
+
+
